d36820d33bc6bf5ee3b742ca64d1aeb1991d54d8
1yul2 <hanyul9824@gmail.com>
2026-01-13 14:04:03 +0900

    ✨feat(#108): 토큰 스키마 정리

--- app/modules/auth/schemas.py
+++ app/modules/auth/schemas.py
@@ -9,6 +9,12 @@ class LoginRequest(BaseModel):
 class TokenResponse(BaseModel):
     is_system: bool
     access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+
+class RefreshRequest(BaseModel):
+    access_token: str
     token_type: str = "bearer"
 
 
