554d6e24be62dcb16b8315bd2c737fd97f30960f
1yul2 <hanyul9824@gmail.com>
2026-01-21 12:52:48 +0900

    ♻️refactor(#129): 스케줄-dayoff,shift 통합

--- app/core/routers.py
+++ app/core/routers.py
@@ -9,8 +9,6 @@ api_router = APIRouter()
 routers = [
     ("/auth", "로그인관리", "app.modules.auth.routers"),
     ("/schedule", "스케줄관리", "app.modules.schedule.routers"),
-    ("/shift", "스케줄관리", "app.modules.shift.routers"),
-    ("/dayoff", "스케줄관리", "app.modules.dayoff.routers"),
     ("/payroll", "급여관리", "app.modules.payroll.router.routers"),
     ("/workstatus", "근태관리", "app.modules.workstatus.routers"),
     ("/community", "커뮤니티관리", "app.modules.community.routers"),
--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -1,10 +1,8 @@
 from app.modules.admin import models as admin_models
 from app.modules.auth import models as auth_models
 from app.modules.community import models as community_models
-from app.modules.dayoff import models as dayoff_models
 from app.modules.payroll import models as payroll_models
 from app.modules.schedule import models as schedule_models
-from app.modules.shift import models as shift_models
 from app.modules.workstatus import models as workstatus_models
 
 # 필요시 계속 추가
@@ -14,8 +12,6 @@ __all__ = [
     "admin_models",
     "payroll_models",
     "schedule_models",
-    "shift_models",
-    "dayoff_models",
     "community_models",
     "workstatus_models",
 ]
