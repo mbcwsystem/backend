f9c2b5833f644861ccfb923b72c8ddc2946d68d2
ji-min0 <k.jimin2002@gmail.com>
2026-02-10 17:23:26 +0900

    :sparkles: feat(#141): cors path 추가
    - 프론트 배포 링크 추가

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ origins = [
     "http://localhost:5173",
     "http://mbansan.iptime.org",
     "http://mbansan.iptime.org:8000",
+    "https://frontend-bay-gamma-33.vercel.app/",
 ]
 
 app.add_middleware(
